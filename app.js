64b0158df1b06db5e3136478fb1c461b002ff0ef
Harrison Favour <fayvorharrison101@gmail.com>
2019-11-16 18:16:56 +0100

    move routes into src folder and added mongoose package

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
 
 var app = express();
 
